da4a0a8b920889e45cb20d9d1a9d520144feee03
Peter Clift <petercli@hotmail.com>
2023-08-03 05:53:19 -0700

    'First_Commit'

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,44 @@
+<script>
+   export const appName ='SvelteKit & World ID'   
+</script>
+<header>
+<nav>
+<ul>
+    <li>
+        <a href="/">Home</a>
+    </li>
+    <li>
+        <a id="itemanchor"  class=  "disable-click"  href="/aritems" data-sveltekit-preload-data="hover">AR Items</a>
+    </li>
+    <li>
+        <a  id="addanchor"  class=  "disable-click"  href="/add" >Add</a>
+    </li>
+    <li>
+        <a href="/about">About</a>
+    </li>
+</ul>
+</nav>
+<h1>{appName}</h1>  
+
+</header>
+
+
+
+<slot/>
+
+<style>
+
+.disable-click{
+          pointer-events:none;
+      }
+      
+nav{
+    display: flex;
+}
+
+nav a{
+    margin-right: 1rem;
+}
+
+
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,42 @@
+<script>
+import { IDKitWidget } from '@worldcoin/idkit'  
+import { browser } from '$app/environment'
+let loginButton='Login'
+
+async function walletlogin() {
+    console.log('*** wallet login2')
+    if (browser) {
+     
+        console.log('*** wallet login2')
+      
+
+
+          
+
+    }
+}
+
+</script>
+
+
+
+<div class="flex space-x-8">
+ <button  class="btn"
+   on:click={async () => {   walletlogin() 
+
+   }}>{loginButton}</button
+ >
+</div>
+
+<style>
+    .btn {
+      padding: 0.8rem 1rem 0.7rem;
+      border: 0.2rem solid #4d4d4d;
+      cursor: pointer;
+      text-transform: capitalize;
+      width: 200px;
+    
+    
+    
+    }
+</style>
