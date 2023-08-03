0dfbbb2b9444dbe9817f9afc44856f7f7f7d51bf
Peter Clift <petercli@hotmail.com>
2023-08-03 06:17:37 -0700

    add about

---
+++ src/routes/about/+page.server.js
@@ -0,0 +1,4 @@
+import { error } from '@sveltejs/kit';
+
+
+ 
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+
+ import { version } from '$app/environment'
+
+
+</script>
+
+ 
+<p></p>
+<h1>About</h1>
+
+ 
+     
+
+
+
+<p>GNU GENERAL PUBLIC LICENSE Version 3</p>
+ 
